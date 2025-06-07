02f8a63119edf07dac9f590bfe3ddce0db835751
DoggiKim <kmss0426@hanyang.ac.kr>
2025-06-08 00:15:32 +0900

    feat: add view more button on archive-summary-section

---
+++ src/Component/Icons/ViewMoreIcon.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function ViewMoreIcon () {
+
+    return(
+        <svg 
+            width="11" 
+            height="10" 
+            viewBox="0 0 11 10" 
+            fill="none" 
+            stroke="#252420"
+            strokeWidth="1.14291"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            xmlns="http://www.w3.org/2000/svg">
+            <path d="M1.49951 5.16443H9.49991"/>
+            <path d="M5.49951 1.16431L9.49971 5.1645L5.49951 9.1647" />
+        </svg>
+
+    )
+}
