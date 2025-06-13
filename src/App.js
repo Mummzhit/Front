e22427c81ec02f9228fcc5ce24660c6bffb554bb
DoggiKim <kmss0426@hanyang.ac.kr>
2025-06-13 17:00:05 +0900

    feat: Complete login and signup pages with temporary routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from 'react';
 
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import MainPage from './Pages/MainPage';
 import BottomNav from './Component/BottomNav/BottomNav';
-import LoginPage from './Pages/LoginPage';
+import LoginOnboardingPage from './Pages/LoginPage/LoginOnboardingPage';
+import LoginPage from './Pages/LoginPage/LoginPage';
+import SignUpPage from './Pages/LoginPage/SignUpPage';
 
 function App() {
   return (
-    <div className="App">
-      <LoginPage/>
-      {/* <MainPage />
-      <BottomNav className='BottomNav' onTabChange={(tabId) => console.log(`Active tab changed to: ${tabId}`)} /> 
-       */}
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/login' element={<LoginPage/>} />
+          <Route path='/' element={<LoginOnboardingPage/>} />
+          <Route path='/main' element={<MainPage/>} />
+          <Route path='/sign-up' element={<SignUpPage/>} />
+        </Routes>
+          {/* <LoginPage/> */}
+          {/* <LoginOnboardingPage/> */}
+          {/* <MainPage />
+          <BottomNav className='BottomNav' onTabChange={(tabId) => console.log(`Active tab changed to: ${tabId}`)} /> 
+          */}
+      </div>
+    </Router>
+    
    
   );
 }
---
+++ src/Pages/LoginPage/SignUpPage.jsx
@@ -0,0 +1,61 @@
+import LoginButton from "../../Component/LoginPage/LoginButton";
+import './LoginPage.css'
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import LoginTextField from "../../Component/LoginPage/LoginTextField";
+import InputField from "../../Component/LoginPage/InputField";
+
+
+export default function SignUpPage () {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [nickname, setNickname] = useState("");
+    const navigate = useNavigate();
+
+    const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    console.log("입력된 이메일:", e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+        console.log("입력된 비밀번호:", e.target.value);
+    };
+
+
+
+    const handleLogin = () => {
+        navigate ('/login');
+        console.log ('login button is pressed')
+    };
+
+    const handleSignUp = () => {
+        navigate('/sign-up');
+        console.log ('sign up button is pressed')
+    };
+
+
+    
+    return (
+
+        <div className="login-page">
+            
+            <div className="title-field">
+                <LoginTextField title='만나서 반가워요' subtitle='원활한 서비스 이용을 위해 계정을 등록해주세요' /> 
+            </div>
+            <div className="input-zone">
+                <InputField type='id' placeholder='Email' onChange={handleEmailChange}/>
+                <InputField type='nickname' placeholder='Nickname'/>
+                <InputField type='password' placeholder='Password' onChange={handlePasswordChange}/>
+            </div>
+            
+            <div className="button-zone">
+                    <LoginButton type = 'login' text ='계정 등록하기' onClick={handleSignUp}/>
+                    <LoginButton type = 'sign-up' text ='로그인하기' onClick={handleLogin}/>
+                </div>
+                
+        </div>
+            
+    )
+}
+    
